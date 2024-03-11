db7dabcf026e8d70eecd279d03a9d96dc416385a
lynxloader <162089130+lynxloader@users.noreply.github.com>
2024-03-11 15:46:25 +0630

    Update tech.lua

--- tech.lua
+++ tech.lua
@@ -349,9 +349,7 @@ spawn(function()
     end
    end)
    
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/lynxloader/Script/main/UIFluents.lua"))()
 
 local Window = Fluent:CreateWindow({
     Title = "VN Hub",
